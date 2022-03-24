9c825e488cb55b5c940f935b7172355dd258b22a
Bhupendra Singh <bhupendrasingh@IT-Admins-MacBook-Pro.local>
2022-03-24 09:07:50 +0530

    Added Unit Test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-  }
-}
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        semi: ['error', 'always'],
+        indent: ['error', 4]
+    },
+    env: { node: true, mocha: true }
+};
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+if(process.env.ENV === 'Test') {
+    console.log('This is a test');
+    const db = mongoose.connect('mongodb://localhost:27017/booksAPI_Test');
+} else {
+    console.log('This is for production environment');
+    const db = mongoose.connect('mongodb://localhost:27017/booksAPI');
+}
+const port = process.env.PORT || 3000;
+
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my Nodemon API')
-})
+    res.send('Welcome to my Nodemon API');
+});
 
-app.listen(port, () => {
-  console.log('Runninn on port ' + port)
-})
+app.server = app.listen(port, () => {
+    console.log('Runninn on port ' + port);
+});
+
+module.exports = app;
